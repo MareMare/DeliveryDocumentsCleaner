00d97a320930fb68a3bf34f9490079ac2dc7ac37
MareMare <MareMare@users.noreply.github.com>
2022-12-28 12:21:45 +0900

    `Disposable` を追加します。

---
+++ dev/Sandbox.Core.UnitTests/DisposableTest.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DisposableTest.cs" company="MareMare">
+// Copyright © 2022 MareMare. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sandbox.Core.UnitTests;
+
+public class DisposableTest
+{
+    [Fact]
+    public void WhenDispose_DisposableCreated_ShouldCall()
+    {
+        var called = false;
+        var disposable = Disposable.Create(() => called = true);
+        Assert.NotNull(disposable);
+        Assert.False(called);
+
+        disposable.Dispose();
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void WhenDispose_DisposableEmpty_DoseNotThrow()
+    {
+        var disposable = Disposable.Empty;
+        Assert.NotNull(disposable);
+        disposable.Dispose();
+    }
+
+    [Fact]
+    public void WhenExpressionIsNull_ThrowException()
+        => Assert.Throws<ArgumentNullException>(() => Disposable.Create(null!));
+}
