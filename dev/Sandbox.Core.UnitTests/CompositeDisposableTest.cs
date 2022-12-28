aa13cb2c248220c2cd8766dc69712748c05bfc36
MareMare <MareMare@users.noreply.github.com>
2022-12-28 12:35:49 +0900

    `CompositeDisposable` を追加します。

---
+++ dev/Sandbox.Core.UnitTests/CompositeDisposableTest.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompositeDisposableTest.cs" company="MareMare">
+// Copyright © 2022 MareMare. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sandbox.Core.UnitTests;
+
+public class CompositeDisposableTest
+{
+    [Fact]
+    public void AddDisposable()
+    {
+        var called = false;
+        var disposableToAdd = Disposable.Create(() => called = true);
+        var disposable = new CompositeDisposable();
+
+        disposable.Add(disposableToAdd);
+        Assert.False(disposable.IsDisposed);
+        Assert.False(called);
+
+        disposable.Dispose();
+        Assert.True(disposable.IsDisposed);
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void AddDisposable_AlreadyDisposed()
+    {
+        var called = false;
+        var disposableToAdd = Disposable.Create(() => called = true);
+        var disposable = new CompositeDisposable();
+
+        disposable.Dispose();
+        Assert.True(disposable.IsDisposed);
+        Assert.False(called);
+
+        disposable.Add(disposableToAdd);
+        Assert.True(disposable.IsDisposed);
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void Ctor_WithEnumerable_DoseNotThrow()
+    {
+        var called1 = false;
+        var disposableToAdd1 = Disposable.Create(() => called1 = true);
+        var called2 = false;
+        var disposableToAdd2 = Disposable.Create(() => called2 = true);
+
+        var disposable = new CompositeDisposable(disposableToAdd1, disposableToAdd2);
+
+        Assert.False(disposable.IsDisposed);
+        Assert.False(called1);
+        Assert.False(called2);
+
+        disposable.Dispose();
+        Assert.True(disposable.IsDisposed);
+        Assert.True(called1);
+        Assert.True(called2);
+    }
+}
---
+++ dev/Sandbox.Core/CompositeDisposable.cs
@@ -0,0 +1,109 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompositeDisposable.cs" company="MareMare">
+// Copyright © 2022 MareMare. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Concurrent;
+
+namespace Sandbox.Core;
+
+/// <summary>
+/// Dispose 可能なインスタンスのコンテナを表します。
+/// </summary>
+public class CompositeDisposable : IDisposable
+{
+    /// <summary>コンテナを表します。</summary>
+    private readonly ConcurrentBag<IDisposable> _disposables;
+
+    /// <summary>
+    /// <see cref="CompositeDisposable" /> クラスの新しいインスタンスを初期化します。
+    /// </summary>
+    /// <param name="disposables">新しいリストに要素がコピーされるコレクション。</param>
+    public CompositeDisposable(params IDisposable[] disposables)
+    {
+        this._disposables = new ConcurrentBag<IDisposable>(disposables) ??
+                            throw new ArgumentNullException(nameof(disposables));
+    }
+
+    /// <summary>
+    /// Dispose されたかを取得します。
+    /// </summary>
+    /// <value>
+    /// 値を表す <see cref="bool" /> 型。
+    /// <para>Dispose された場合 true。既定値は false です。</para>
+    /// </value>
+    public bool IsDisposed { get; private set; }
+
+    /// <summary>
+    /// Dispose 可能なインスタンスのコンテナを生成します。
+    /// </summary>
+    /// <param name="disposables">新しいリストに要素がコピーされるコレクション。</param>
+    /// <returns>生成された <see cref="IDisposable" /> を実装したインスタンス。</returns>
+    public static IDisposable Create(params IDisposable[] disposables) => new DisposableArray(disposables);
+
+    /// <summary>
+    /// コンテナに追加します。
+    /// </summary>
+    /// <param name="disposable">追加するインスタンス。</param>
+    public void Add(IDisposable disposable)
+    {
+        if (this.IsDisposed)
+        {
+            disposable.Dispose();
+        }
+        else
+        {
+            this._disposables.Add(disposable);
+        }
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        if (this.IsDisposed)
+        {
+            return;
+        }
+
+        this.IsDisposed = true;
+        while (this._disposables.TryTake(out var disposable))
+        {
+            disposable.Dispose();
+        }
+
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Dispose 可能なインスタンスのコンテナを表します。
+    /// </summary>
+    private sealed class DisposableArray : IDisposable
+    {
+        /// <summary>コンテナを表します。</summary>
+        private IDisposable[]? _disposables;
+
+        /// <summary>
+        /// <see cref="DisposableArray" /> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="disposables">新しいリストに要素がコピーされるコレクション。</param>
+        public DisposableArray(IDisposable[] disposables)
+        {
+            Volatile.Write(ref this._disposables, disposables ?? throw new ArgumentNullException(nameof(disposables)));
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            var old = Interlocked.Exchange(ref this._disposables, null);
+            if (old != null)
+            {
+                foreach (var disposable in old)
+                {
+                    disposable?.Dispose();
+                }
+            }
+        }
+    }
+}
